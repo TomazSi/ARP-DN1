1522e5e5aa8ad8e2207c1d679f8241b8903fa7e2
tomaz <tomaz.simunic@student.um.si>
2024-03-31 12:31:49 +0200

    Adapted the previous code to only use 2 args and deleted unnecesary functions

--- main.cpp
+++ main.cpp
@@ -28,86 +28,18 @@ void Izpis_Stevil(int* polje, unsigned int velikost) {
 		output << polje[i] << ' ';
 }
 
-void PodporaNegativnih(vector<int> &A) {
-    int minVal = A[0];
-    for (int i = 1; i < A.size(); i++) {
-        if (A[i] < minVal) {
-            minVal = A[i];
-        }
-    }
-
-    if (minVal < 0) {
-        for (int i = 0; i < A.size(); i++) {
-            A[i] -= minVal;
-        }
-    } else {
-        minVal = 0;
-    }
-
-    Minimum = minVal;
-}
-
-void RePodporaNegativnih(vector<int> &A) {
-    for (int i = 0; i < A.size(); i++) {
-        A[i] += Minimum;
-    }
-}
-void CountingSort(vector<int>& A) {
-    int maxVal = *max_element(A.begin(), A.end());
-    vector<int> C(maxVal + 1, 0);
-    vector<int> B(A.size());
-
-    for (int i = 0; i < A.size(); i++) {
-        C[A[i]]++;
-    }
-
-    for (int i = 1; i <= maxVal; i++) {
-        C[i] += C[i - 1];
-    }
-
-    for (int i = A.size() - 1; i >= 0; i--) {
-        B[C[A[i]] - 1] = A[i];
-        C[A[i]]--;
-    }
-
-    A = B;
-}
-
-void RomanSort(vector<int>& A) {
-    int maxVal = *max_element(A.begin(), A.end());
-    vector<int> C(maxVal + 1, 0);
-    vector<int> B(A.size());
-
-    for (int i = 0; i < A.size(); i++) {
-        C[A[i]]++;
-    }
-
-    int idx = 0;
-    for (int i = 0; i <= maxVal; i++) {
-        for (int j = 0; j < C[i]; j++) {
-            B[idx++] = i;
-        }
-    }
-
-    A = B;
-}
-
-
 int main(int argc, const char* argv[]) {
 	vector<int> A;
 
-	if (argc < 3) return 0;
-	if (!Branje_Stevil(A, argv[2])) return 0;
-    PodporaNegativnih(A);
-	if (argv[1][0] == '0') {
-		//counting sort
-        CountingSort(A);
-	}
-	else {
-		//Roman sort
-        RomanSort(A);
-	}
-    RePodporaNegativnih(A);
+	if (argc < 2) {
+        cout << "Usage: dn1 <input_file>" << endl;
+        return 0;
+    }
+
+    if (!Branje_Stevil(A, argv[1])) {
+        cout << "Error reading input file." << endl;
+        return 0;
+    }
 	Izpis_Stevil(&A[0],A.size());
 
 	return 0;
