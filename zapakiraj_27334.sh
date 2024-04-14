4c21532bf0a57267937ecc57c0e6d0809c7351fd
tomaz <tomaz.simunic@student.um.si>
2024-04-14 21:55:54 +0200

    fixx

--- zapakiraj_27334.sh
+++ zapakiraj_27334.sh
@@ -1,1 +1,10 @@
-zip -r docker_27334.zip . -x "*.git*" -x "*.zip" -x "*.tar.gz"
+#!/bin/bash
+
+# Preveri, ali obstaja Dockerfile
+if [ ! -f "Dockerfile" ]; then
+    echo "Napaka: Dockerfile ne obstaja"
+    exit 1
+fi
+
+# Zgradi Docker sliko iz Dockerfile-a
+docker build -t tomazsi/ghactionsarp .
